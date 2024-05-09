a70104e5451fd86918261877ffa7337442d2869c
SOHAIL1996 <sorox23@gmail.com>
2024-05-09 20:19:15 +0200

    More basic practice

--- project_1/tutorial.cpp
+++ project_1/tutorial.cpp
@@ -1,7 +1,83 @@
-#include <iostream>
+/*
+Multi Line Comment
+*/
 
-int main() // Executor First Called
+#include <iostream> // Location matters and is restricted to the curly bracket space
+
+int power(double, int); // Function Declaration Only -> Useful so you can place you function anywhere without worrying about compile errors
+
+int power(double base, int exponent) // Function Decalaration and Initialization
 {
-    std::cout << "Test\n";
-    return 0;
+    double result =1;
+
+    for(int i = 0; i<exponent; i++){
+        result = result*base;
+    }
+    return result;
+}
+
+int add(int x, int y) // Function Decalaration and Initialization
+{
+   return x+y;
+}
+
+int main() // Executor First Called Automatically
+{
+    int guava;
+    int mango, dragonfruit;
+    int fruit1= 0;
+    int fruit2=0;
+    int cookies = 10; // Declaration and Initialization
+
+
+    int melons; // Declaration  
+    melons = 20; // Initialization
+
+
+    std::cout << "Test\n"; // std -> Standard Library
+
+    std::cout << "Input Guava Numbers\n"; 
+    std::cin >> guava;   
+
+    std::cout << "Fruit 1 and Fruit2\n";
+    std::cin >> fruit1; 
+    std::cin >> fruit2;    
+    std::cout << "Fruits added are: "<< add(fruit1, fruit2) << std::endl;
+
+    int fruit_addition = add(fruit1, fruit2);
+    printf("%i fruit_addition\n", fruit_addition); // Requires iostream
+
+    std::cout << "Melon amount: "<< melons << std::endl;
+
+    printf("Melons with printf: %i \n", melons); // Requires iostream
+    printf("Power function %d \n",power(fruit1,fruit2));
+    std::cout << "Test2 \n";
+
+    return 0; 
+}
+
+void tests() 
+{
+    std::cout << "Cookies???\n"; 
+}
+//----------------------------------------------------------------------------------
+// V2 Using Namespace for Global Removal -> Not Recommended for large code bases
+//----------------------------------------------------------------------------------
+
+int main2() 
+{   
+    using namespace std;
+    cout << "Test\n"; 
+    return 0; 
+}
+
+//----------------------------------------------------------------------------------
+// V3 Using Namespace for Global Removal -> Can be used and is safer then v1
+//----------------------------------------------------------------------------------
+
+int main3() 
+{
+    using std::cout;
+    cout << "Test\n"; 
+    return 0; 
 }
---
+++ project_1/tutorial2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Location matters and is restricted to the curly bracket space
+
+void main() // Executor First Called
+{
+    int melons; // Declaration  
+    melons = 20; // Initialization
+
+    printf("%i asdasd\n", melons);
+ 
+}
+
