20595999ce70b7a95191fa5cb2e735d086fd49ce
SOHAIL1996 <sorox23@gmail.com>
2024-07-08 20:24:54 +0200

    Additional practice

--- project_1/tutorial2.cpp
+++ project_1/tutorial2.cpp
@@ -5,7 +5,22 @@ void main() // Executor First Called
     int melons; // Declaration  
     melons = 20; // Initialization
 
-    printf("%i asdasd\n", melons);
- 
-}
+    auto mango=43; // Automatically assigns variable (default double)
+    auto mangou=43ULL; // Automatically assigns variable with unisgned long long
+    auto mangof=43.0F; // Automatically assigns variable as float
 
+    printf("%i asdasd\n", melons);
+    printf("%i mango\n", mango);
+    printf("%i mangou\n", mangof); 
+
+    int number = 30; // Base 10
+    int number_hex = 0x30; // base 16
+    int number_oct = 030; // base 8
+
+    std::cout << number << std::endl;
+    std::cout << number_hex << std::endl;
+    std::cout << number_oct << std::endl;
+    
+    std::cout << std::hex <<number << std::endl;
+    std::cout << std::oct <<number << std::endl;
+}
--- project_1/tutorial6.cpp
+++ project_1/tutorial6.cpp
@@ -9,14 +9,27 @@ void main()
     string gtt = "lol"; // string is empty by default no error
     gtt += "!"; 
     string user_input;
+    string user_input2;
 
     cout << gtt << endl;
-    cout << gtt.length() << endl;
+    cout << gtt.length() << endl; 
+    cout << gtt.size() << endl; // Same as length 
     cout << gtt [1] << endl;
     cout << gtt + " remove" << endl; // directly add
+    gtt+=" cookies"; // Directly modifies the string
+    gtt.append(" two cookies"); // Same like append
+    gtt.erase(2,1); // Erase character in provided indexes 
+    gtt.erase(2,1); // Erase all characters after this index
+    gtt.erase(gtt.length()-1); // Erase last character
+    gtt.replace(0,2,"repplla"); // Replace character from index to index
+    gtt.insert(2," noiice "); // Inserts character in provided index 
+    cout << gtt << endl;
 
-    cin >> user_input; // Gets only the first word                                                                             
+    // cin >> user_input; // Gets only the first word                                                                             
 
     char string_variable[]="cookies"; // Can also use array technique -> This is c style, size cant be changed
     cout << string_variable << endl;
+
+    getline(cin, user_input2); // Gets line
+    cout << user_input2 << endl;
 }
